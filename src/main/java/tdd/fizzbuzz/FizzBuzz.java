477c57aeb2515ffa746a61d7cf69308edf902c86
Bryan Jay Zacarias <zacarbr@oocl.com>
2022-09-01 14:22:29 +0800

    reafactor: Declaring the Fizz as a variable

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -4,9 +4,10 @@ import static java.lang.Integer.valueOf;
 
 public class FizzBuzz {
     public String countOff(int order) {
+        String FIZZ = "Fizz";
 
         if (order % 3 == 0) {
-            return "Fizz";
+            return FIZZ;
         }
         return String.valueOf(order);
     }
