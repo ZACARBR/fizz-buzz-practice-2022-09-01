6d6b83436f9dd8029dc5d4c5455e4467b8f6cbb1
Bryan Jay Zacarias <zacarbr@oocl.com>
2022-09-01 14:25:24 +0800

    feat: Adding the implementation if the number is divisible by number 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -9,6 +9,11 @@ public class FizzBuzz {
         if (order % 3 == 0) {
             return FIZZ;
         }
+
+        if (order % 5 == 0){
+            return "Buzz";
+        }
+
         return String.valueOf(order);
     }
 }
