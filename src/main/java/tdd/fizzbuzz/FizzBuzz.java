5530972562819083ede6d4158d53ce8ba3b42127
Bryan Jay Zacarias <zacarbr@oocl.com>
2022-09-01 14:07:24 +0800

    test: should_return_1_when_count_off_given_number_1 in normal case

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,7 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    public Integer countOff(int i) {
+        return null;
+    }
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,22 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+
+    @Test
+    void should_return_1_when_count_off_given_number_1() {
+        //given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        Integer expected = 1;
+        //when
+        Integer actual = fizzBuzz.countOff(1);
+
+        //then
+        assertEquals(expected, actual);
+    }
+
+
 }
