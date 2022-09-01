0dde440af140fdbe1c7738b47f2fb56834e43891
Bryan Jay Zacarias <zacarbr@oocl.com>
2022-09-01 14:26:38 +0800

    refactor: Declaring the Buzz as a variable

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -5,13 +5,14 @@ import static java.lang.Integer.valueOf;
 public class FizzBuzz {
     public String countOff(int order) {
         String FIZZ = "Fizz";
+        final String buzz = "Buzz";
 
         if (order % 3 == 0) {
             return FIZZ;
         }
 
         if (order % 5 == 0){
-            return "Buzz";
+            return buzz;
         }
 
         return String.valueOf(order);
